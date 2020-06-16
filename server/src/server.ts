5a65a1c6358b8fb8d8bd85efc11d96a53ec17bfe
alissongoulart <alissonbrenoemp@gmail.com>
2020-06-16 01:43:58 -0300

    Second day [Next Level Week] - Starting Back-End

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,13 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários.');
+app.use(express.json());
+app.use(routes);
 
-    response.json([
-        'Alisson',
-        'Eduardo',
-        'Roni',
-        'Jão Ed',
-        'Mais um :D'
-    ]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
 
 app.listen(3333);
- 
